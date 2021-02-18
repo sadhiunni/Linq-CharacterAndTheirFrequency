606d18917ee0f0ac510389858138d3c19d237691
sadhiunni <76890340+sadhiunni@users.noreply.github.com>
2021-02-18 22:06:27 +0530

    Add project files.

---
+++ Linq-CharacterAndTheirFrequency/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+/*To find the frequency of each character in an inputed string. 
+ */
+namespace Linq_CharacterAndTheirFrequency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the string : ");
+            var input = Console.ReadLine();
+
+            var CharacFreq = from x in input
+                             group x by x into y
+                             select y;
+
+            foreach (var character in CharacFreq)
+            {
+                Console.WriteLine("The frequency of {0} in the {1} is {2} ",character.Key, input, character.Count()) ;
+            }
+
+        }
+    }
+}
